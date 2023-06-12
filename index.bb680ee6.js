f85b1dd1121b4ae4f0b3cb977e7426ddad33c333
DavydiukDmytro <69296072+DavydiukDmytro@users.noreply.github.com>
2023-06-12 12:47:48 +0000

    Deploying to gh-pages from @ DavydiukDmytro/canvas-test@0a82c0bdd9cbc8b582c28e90bc43633bd7179c6c 🚀

---
+++ index.bb680ee6.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("canvas").getContext("2d");e.beginPath(),e.rect(20,40,50,50),e.fillStyle="#FF0000",e.fill(),e.closePath(),e.beginPath(),e.arc(240,160,20,0,2*Math.PI,!1),e.fillStyle="green",e.fill(),e.closePath(),e.beginPath(),e.rect(160,10,100,40),e.strokeStyle="rgba(0, 0, 255, 0.5)",e.stroke(),e.closePath();
+//# sourceMappingURL=index.bb680ee6.js.map
---
+++ index.e145087c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("canvas").getContext("2d");e.beginPath(),e.rect(20,40,50,50),e.fillStyle="#FF0000",e.fill(),e.closePath(),e.beginPath(),e.arc(240,160,20,0,2*Math.PI,!1),e.fillStyle="green",e.fill(),e.closePath(),e.beginPath(),e.rect(160,10,100,40),e.strokeStyle="rgba(0, 0, 255, 0.5)",e.stroke(),e.closePath()}();
+//# sourceMappingURL=index.e145087c.js.map
